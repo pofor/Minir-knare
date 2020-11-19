8f924203e2136f8bcc5e5db1b9f7d17def162899
pofor <54797719+pofor@users.noreply.github.com>
2020-11-19 10:00:21 +0100

    funkar inte än
    jag har gjort så att alla knappar funkar förutom räkne sätten det ska jag göra nu med en split.

---
+++ Miniräknare/Miniräknare/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Miniräknare
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        public void OkButton_Klick(object sender, RoutedEventArgs e)
+        {
+            if (e.Source is Button button)
+            {
+                OkText.Text += button.Content;
+            }
+            
+        }
+        private void equal_Click(object sender, EventArgs e)
+        {
+            double num1, num2;
+            if (Operation == )
+            {
+
+            }
+        }
+    }
+}
